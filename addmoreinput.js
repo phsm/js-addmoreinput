d3268e543ad99d5ebb2ae13037e75c8fb5970902
Stanislaw Datskevich <me@nek0.net>
2016-02-01 13:16:44 +0200

    Added max entries parameter

--- addmoreinput.js
+++ addmoreinput.js
@@ -1,7 +1,12 @@
-function moreInputInit(inputs_div_id, input_html) {
+function moreInputInit(inputs_div_id, input_html, max_entries) {
+    if (!max_entries) max_entries = 64;
+    var i = document.getElementById(inputs_div_id).getElementsByClassName('addmore-span').length;
     document.querySelector('#' + inputs_div_id + ' .addmore-button').addEventListener('click', function (e) {
         e.preventDefault();
-        document.querySelector('#' + inputs_div_id).insertAdjacentHTML('beforeend', '<span class="addmore-span">' + input_html + '</span>');
+        if ( i < max_entries ) {
+            document.querySelector('#' + inputs_div_id).insertAdjacentHTML('beforeend', '<span class="addmore-span">' + input_html + '</span>');
+            i++;
+        }
     });
 
     document.querySelector('#'+inputs_div_id).addEventListener('click', function(e) {
@@ -9,5 +14,6 @@ function moreInputInit(inputs_div_id, input_html) {
         if (!targetElement.classList.contains('addmore-remove-button')) return;
         e.preventDefault();
         targetElement.parentElement.remove();
+        i--;
     });
 }
